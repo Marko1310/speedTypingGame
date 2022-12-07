e38a2fb3e840c237e6b3bcb1d7189dfa6b1cc60b
Marko1310 <marko.cabo13@gmail.com>
2022-12-07 21:44:12 +0100

    Logic to count words

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const handleChange = function (e) {
+    setText(e.target.value);
+  };
+
+  const calculateWordCount = function (text) {
+    const wordsArr = text.trim().split(" ");
+    return wordsArr.filter((word) => word !== "").length;
+  };
+
   return (
     <div>
       <h1>How fast can you type</h1>
-      <textarea />
+      <textarea onChange={handleChange} />
       <h4>Time remaining: </h4>
-      <button>Start</button>
+      <button onClick={() => calculateWordCount(text)}>Start</button>
       <h1>Word count:</h1>
     </div>
   );
